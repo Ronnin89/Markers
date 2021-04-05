7c8414797cb1fbb7314bdd7cd12e062c85da6e79
Ricardo Alvarez <ri.alvarez.89@icloud.com>
2021-04-05 10:44:32 -0400

    initial commit

---
+++ db/migrate/20210405144058_create_categories.rb
@@ -0,0 +1,11 @@
+class CreateCategories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :categories do |t|
+      t.string :name
+      t.boolean :private
+      t.references :categories, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
